fb746aac3addb3fa6f926eca976308be684f7268
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2022-06-01 12:04:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//Beni Yaratan Elbet Yolumu Gösterir .Şuara78
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float number1;
+            number1 = 7.67f;
+
+            Console.Write("Float number is :" + " " + number1);
+
+            Console.Read();
+
+
+        }
+    }
+}
